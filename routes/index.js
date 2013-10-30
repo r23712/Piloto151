2f879b5c28d084985c3d073a9842c6c7e3cfc05b
elbuo8 <yamil.asusta@upr.edu>
2013-10-30 14:43:20 -0400

    Added csrf, mongo connection, and the routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
-
+require('mongodb').MongoClient('mongodb://localhost:27017/piloto151',
+  {native_parser: true}, function (e, db) {
+    this.db = db;
+  });
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function (req, res) {
+  res.render('index');
+};
+
+exports.check = function (req, res) {
+
+};
+
+exports.save = function (req, res) {
+
 };
