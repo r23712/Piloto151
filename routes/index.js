ae18357055a861e08f914b28d2fef288e876da15
elbuo8 <yamil.asusta@upr.edu>
2013-10-31 03:25:14 -0400

    Devopsy stuff and logic on how to evaluate code submission

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,39 @@
-require('mongodb').MongoClient('mongodb://localhost:27017/piloto151',
+require('mongodb').MongoClient.connect('mongodb://localhost:27017/piloto151',
   {native_parser: true}, function (e, db) {
+    if (e) {
+      console.log(e);
+      process.exit();
+    }
     this.db = db;
   });
-/*
- * GET home page.
- */
+require('fs').readFile('./fizzbuzz', function (e, solution) {
+  if (e) {
+    console.log(e);
+    process.exit();
+  }
+  this.solution = solution;
+});
+
+var exec = require('child_process').exec;
 
 exports.index = function (req, res) {
   res.render('index');
 };
 
+function save(req, res) {
+
+}
+
 exports.check = function (req, res) {
-
+  /*
+  if (req.param('language') === 'java') {
+    });
+  } else {
+    exec()
+  }
+  exec(command, function (e, stout, stde) {
+    if (stdout === solution) res.send(200);
+    else res.send(400);
+  */
 };
 
-exports.save = function (req, res) {
-
-};
