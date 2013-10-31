2c8865dacd6223982840a7c5799be73314af7d2f
elbuo8 <yamil.asusta@upr.edu>
2013-10-31 17:13:00 -0400

    Almost done bitch

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.configure('development', function () {
 
 
 app.get('/', csrf, routes.index);
-app.post('/check', routes.check);
+app.post('/save', csrf, routes.check);
 
 http.createServer(app).listen(app.get('port'), function () {
   console.log("Express server listening on port " + app.get('port'));
