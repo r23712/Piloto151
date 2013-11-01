2ad4166383c4a71932d72fed8551ff929f332b01
elbuo8 <yamil.asusta@upr.edu>
2013-11-01 13:50:57 -0400

    Allowing all submissions

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.configure('development', function () {
 
 
 app.get('/', csrf, routes.index);
-app.post('/save', csrf, routes.check);
+app.post('/save', csrf, routes.save);
 
 http.createServer(app).listen(app.get('port'), function () {
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,63 +1,11 @@
 var fs = require('fs');
 var exec = require('child_process').exec;
-var client = require('knox').createClient({
-  key: 'AKIAJYUKSW5FNKZQWI2Q',
-  secret: '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',
-  bucket: 'piloto151-resume'
-});
 
-require('mongodb').MongoClient.connect('mongodb://localhost:27017/piloto151',
-  {native_parser: true}, function (e, db) {
-    if (e) {
-      console.log(e);
-      process.exit();
-    }
-    this.db = db;
-  });
-
-fs.readFile('./fizzbuzz', function (e, solution) {
-  if (e) {
-    console.log(e);
-    process.exit();
-  }
-  this.solution = solution.toString();
-});
 
 exports.index = function (req, res) {
   res.render('index');
 };
 
-function save(req) {
-  var s3 = client.put(req.files.resume.path, { 'x-amz-acl': 'public-read' });
-  s3.on('response', function (res) {
-    if (res.statusCode === 200) {
-      req.body.resume = req.url;
-    }
-    this.db.collection('applicants', function (e, collection) {
-      collection.insert(req.body);
-    });
-  });
-}
+exports.save = function (req, res) {
 
-exports.check = function (req, res) {
-  delete req.body._csrf;
-  var filename = Math.floor((Math.random() * 1000) + 1);
-  fs.writeFile('./' + filename, req.body.code, function (e) {
-    if (e) {
-      res.send(500);
-    }
-    exec('node ' + filename, function (e, stout, stde) {
-      fs.unlink('./' + filename, function (error) {
-        if (e) {
-          res.send(500);
-        }
-        if (stout === this.solution) {
-          save(req);
-          res.send(200);
-        } else {
-          res.send(400);
-        }
-      });
-    });
-  });
 };
